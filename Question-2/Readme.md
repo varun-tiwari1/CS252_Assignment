b525c6227756bec09e5b385f51ace2417624460b
Varun <varuntiwari1501@gmail.com>
2022-11-05 17:06:40 +0530

    Update Readme

--- Question-2/Readme.md
+++ Question-2/Readme.md
@@ -1,1 +1,26 @@
-jcvwe
+## Question-4.22-Solution
+
+### The Code for this Question is - 
+[Threaded_Program.c](https://github.com/varun-tiwari1/CS252_Assignment/blob/master/Question-2/Threaded_Program.c)
+
+## Implementation
+
+- The program for calculating the average, minimum and maximum value among given values is written in c programming language.
+
+
+
+## References
+
+- C code for calculating average,minimum and maximum - StackOverflow 
+
+- For understanding concept of Threads and Multithreading - CS Minor Lectures by Mr. Mohit P Tahiliani and textbook
+
+- Websites - gfg,stackoverflow,man7.org,etc
+
+- For understanding commands and syntax- websites like gfg,includehelp,etc
+
+- For assistance Satyam Kalyane- 201IT269 and Lohith N- 201IT1
+
+
+
+
---
+++ Question-2/main.c
@@ -0,0 +1,88 @@
+#include <stdio.h> 
+#include <stdlib.h>
+#include <pthread.h>
+
+/*  Global Variables    */
+float array[100];
+int count;
+int i;
+int threads[3];
+
+float average;
+float minimum;
+float maximum;
+
+void input()
+{
+    printf("Enter the number of elements: ");
+    scanf("%d",&count);
+    for(i=0;i<count;i++)
+    {
+        scanf("%f",& array[i]);
+    }
+}
+
+/* Thread-1 */
+void *thread_average()
+{
+    float sum=0;
+    for(i=0;i<count;i++)
+    {
+        sum  = sum + array[i];
+    }
+    average = sum/count ;
+    printf("\n Average is %f",average);
+}
+
+/*  Thread-2    */
+void *thread_minimum()
+{
+    float temp;
+    temp = array[0];
+    for(i=0;i<count;i++)
+    {
+        if(array[i]<temp)
+        {
+            temp = array[i];
+        }
+    }
+    minimum = temp;
+    printf("\n Minimum is %f",minimum);
+}
+
+/*  Thread-3    */
+void *thread_maximum()
+{
+    float temp;
+    temp = array[0];
+    for(i=0;i<count;i++)
+    {
+        if(array[i]>temp)
+        {
+            temp = array[i];
+        }
+    }
+    maximum = temp;
+    printf("\n Maximum is %f",maximum);
+}
+
+int main(void)
+{
+    input();
+    
+    pthread_t t1;
+    pthread_t t2;
+    pthread_t t3;
+    
+   threads[0]=pthread_create(&t1,NULL,&thread_average,NULL);
+	
+   threads[1]=pthread_create(&t2,NULL,&thread_minimum,NULL);
+	
+   threads[2]=pthread_create(&t3,NULL,&thread_maximum,NULL);
+
+    pthread_join(t1,NULL);
+
+    pthread_join(t2,NULL);
+
+    pthread_join(t3,NULL);
+}
